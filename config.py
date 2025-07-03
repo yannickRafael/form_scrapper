7df250245d66e3da51c782bb451ac597dd121a5b
yannickRafael <yannickrafael286@gmail.com>
2025-07-04 00:14:48 +0200

    added question type dict

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config():
+    
+    QUESTION_TYPES = {
+        0: "Short answer",
+        1: "Paragraph",
+        2: "Multiple choice",
+        3: "Dropdown",
+        4: "Checkboxes",
+        5: "Linear scale",
+        6: "Multiple choice grid",
+        7: "Checkbox grid",
+        8: "Date",
+        9: "Time"
+    }
